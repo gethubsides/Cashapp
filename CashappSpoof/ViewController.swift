fcd4c844313038ae628de45ca6e2c055c1e075ab
Ethan <keiserea@gmail.com>
2021-12-10 14:29:49 -0500

    Initial Commit

---
+++ CashappSpoof/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashappSpoof
+//
+//  Created by Ethan Keiser on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
